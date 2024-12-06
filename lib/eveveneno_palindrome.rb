2dba6e385e389a1c70d7195372f94a56f17fcdaf
Evelyn R <evelynrodriguezc0@gmail.com>
2024-12-05 23:45:01 -0500

    factor into module, add integer palindrome detection

--- lib/eveveneno_palindrome.rb
+++ lib/eveveneno_palindrome.rb
@@ -1,16 +1,23 @@
 require_relative "eveveneno_palindrome/version"
 
-class String
-
-  # Returns true for a palindrome, false otherwise.  def palindrome?
-  def palindrome?
-  processed_content == processed_content.reverse
-  end
-
-  private
-
-    # Returns content for palindrome testing.
-    def processed_content
-      self.scan(/[a-z]/i).join.downcase
+module EvevenenoPalindrome
+    # Returns true for a palindrome, false otherwise.  def palindrome?
+    def palindrome?
+      processed_content == processed_content.reverse
     end
+
+      private
+
+        # Returns content for palindrome testing.
+        def processed_content
+          self.to_s.scan(/[a-z\d]/i).join.downcase
+        end
+end
+
+class String
+  include EvevenenoPalindrome
+end
+
+class Integer
+  include EvevenenoPalindrome
 end
